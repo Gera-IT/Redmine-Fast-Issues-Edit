4be3f2b0de4143b230c557a46ad759e9964e305b
Alex Sinelnikov <sinelniko@gmail.com>
2014-12-04 15:57:45 +0200

    added error handling if plugin didnt work correctly

--- assets/javascripts/overlay.js
+++ assets/javascripts/overlay.js
@@ -1,41 +1,33 @@
 $(function(){
-
-    selector = $('td.subject').find('a');
-
-
-    $('window').on('click', selector, function(e) {
-        //do something
-    });
-
-
     $('td.subject').find('a').live('click', function(e){
+       e.preventDefault();
 
-           e.preventDefault();
-           id = $(this).attr('href').replace( /\/issues\//, '' );
-           $.ajax({
-               url: "/render_form.js",
-               data: {id: id},
-               dataType: 'script',
-               error: function(XMLHttpRequest, textStatus, errorThrown) {
-                   alert("Status: " + textStatus); alert("Error: " + errorThrown)}
-           })
+       id = $(this).attr('href').replace( /\/issues\//, '' );
+       $.ajax({
+           url: "/render_form.js",
+           data: {id: id},
+           dataType: 'script',
+           error: function(XMLHttpRequest, textStatus, errorThrown) {
+               window.location = $(this).attr('href');
+               }
+       })
    });
 });
 
 
 $(function(){
-
     $('a.new-issue').click(function(e){
-            e.preventDefault();
-            id = $('form').first().attr('action').replace( /\/projects\//, '' );
-            id = id.replace( /\/search/, '' );
-            $.ajax({
-                url: "/render_new_form.js",
-                data: {project_id: id},
-                dataType: 'script',
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert("Status: " + textStatus); alert("Error: " + errorThrown)}
-            })
+        e.preventDefault();
+        id = $('form').first().attr('action').replace( /\/projects\//, '' );
+        id = id.replace( /\/search/, '' );
+        $.ajax({
+            url: "/render_new_form.js",
+            data: {project_id: id},
+            dataType: 'script',
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                window.location = $(this).attr('href');
+            }
+        })
 
     });
 });
